0448de3e4345c87283a4a5faee0fff482763aa4e
Terje Sannum <terje.sannum@nav.no>
2023-05-03 07:08:43 +0200

    change json endpoints to /homes

--- main.go
+++ main.go
@@ -123,7 +123,7 @@ func main() {
 				log.Printf("Live measurements not available for home %v\n", s.Id)
 			}
 			h.UpdatePrices(ctx, client)
-			http.HandleFunc(fmt.Sprintf("/home/%s/prices", h.Id), h.GetPricesHandler)
+			http.HandleFunc(fmt.Sprintf("/homes/%s/prices", h.Id), h.GetPricesHandler)
 			ticker := time.NewTicker(time.Minute)
 			quit := make(chan struct{})
 			go func() {
