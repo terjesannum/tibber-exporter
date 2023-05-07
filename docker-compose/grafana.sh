db02ba5e8ad38992e608eab50ce5bdb68f52da6b
Terje Sannum <terje.sannum@nav.no>
2023-05-07 11:52:04 +0200

    prices dashboard don't require prometheus datasource

--- docker-compose/grafana.sh
+++ docker-compose/grafana.sh
@@ -5,5 +5,5 @@
 mkdir -p /var/lib/grafana/dashboards
 cat /dashboard.json | sed -e 's/${DS_PROMETHEUS}/1/;s/now-7d/now-6h/;s/\"refresh\": \"1m\"/\"refresh\": \"5s\"/' > /var/lib/grafana/dashboards/dashboard.json
 cat /dashboard-without-pulse.json | sed -e 's/${DS_PROMETHEUS}/1/' > /var/lib/grafana/dashboards/dashboard-without-pulse.json
-cat /dashboard-prices.json | sed -e 's/${DS_PROMETHEUS}/1/;s/${DS_TIBBER-EXPORTER}/2/' > /var/lib/grafana/dashboards/dashboard-prices.json
+cat /dashboard-prices.json | sed -e 's/${DS_TIBBER-EXPORTER}/2/' > /var/lib/grafana/dashboards/dashboard-prices.json
 /run.sh
